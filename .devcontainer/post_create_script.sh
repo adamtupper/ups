e610c88d6a6d0220eb54f023afb78e219ffed231
Adam <adam.tupper.1@ulaval.ca>
2023-01-13 20:10:30 +0000

    Update dependencies to minimum Python 3.8 versions.

--- .devcontainer/post_create_script.sh
+++ .devcontainer/post_create_script.sh
@@ -6,3 +6,6 @@ cd ..
 # Setup virtual environment
 virtualenv env -p python3.8
 source env/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt --find-links https://download.pytorch.org/whl/torch_stable.html
