4056c3e59a140be47c8bafab5f30998faaaf4492
Adam <adam.tupper.1@ulaval.ca>
2023-01-15 20:06:24 +0000

    Fix checksum check filepath.

--- scripts/cc_download_cifar10.sh
+++ scripts/cc_download_cifar10.sh
@@ -4,4 +4,4 @@ export VAR_MD5=c58f30108f718f92721af3b95e74349a
 mkdir -p $project/data
 
 /usr/bin/wget -O $project/data/cifar-10-python.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-echo "$VAR_MD5  ./cifar-10-python.tar.gz" | md5sum -c -
+echo "$VAR_MD5  $project/data/cifar-10-python.tar.gz" | md5sum -c -
