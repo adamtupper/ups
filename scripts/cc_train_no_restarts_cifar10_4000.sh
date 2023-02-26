946f029df66e97650fdd35da5dc35d880ce52a3c
Adam <adam.tupper.1@ulaval.ca>
2023-02-26 19:05:21 +0000

    Fix check for experiment ID.

--- scripts/cc_train_no_restarts_cifar10_4000.sh
+++ scripts/cc_train_no_restarts_cifar10_4000.sh
@@ -16,9 +16,9 @@ fi
 
 # Check for (optional) experiment ID for resuming a previous training job
 if [ -z "$2" ]; then
-    exp_id=$2
-else
     exp_id="UPS_$SLURM_ARRAY_JOB_ID"
+else
+    exp_id=$2
 fi
 
 # Print Job info
@@ -30,48 +30,48 @@ echo "Job Array ID / Job ID: $SLURM_ARRAY_JOB_ID / $SLURM_JOB_ID"
 echo "This is job $SLURM_ARRAY_TASK_ID out of $SLURM_ARRAY_TASK_COUNT jobs."
 echo ""
 
-module purge
+# module purge
 
-# Copy data and code to compute node
-mkdir $SLURM_TMPDIR/data
-tar cf $project/ups.tar.gz $project/ups
-tar xf $project/ups.tar.gz -C $SLURM_TMPDIR/ups
-tar xf $project/data/cifar-10-python.tar.gz -C $SLURM_TMPDIR/data
+# # Copy data and code to compute node
+# mkdir $SLURM_TMPDIR/data
+# tar cf $project/ups.tar.gz $project/ups
+# tar xf $project/ups.tar.gz -C $SLURM_TMPDIR/ups
+# tar xf $project/data/cifar-10-python.tar.gz -C $SLURM_TMPDIR/data
 
-cd $SLURM_TMPDIR/ups
+# cd $SLURM_TMPDIR/ups
 
-# Create virtual environment
-module load python/3.8.10 cuda cudnn
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index --upgrade pip
-pip install --no-index -r cc_requirements.txt
+# # Create virtual environment
+# module load python/3.8.10 cuda cudnn
+# virtualenv --no-download $SLURM_TMPDIR/env
+# source $SLURM_TMPDIR/env/bin/activate
+# pip install --no-index --upgrade pip
+# pip install --no-index -r cc_requirements.txt
 
-if test -d "$scratch/$exp_id"; then
-    # Resume training run
-    python train-cifar-without-restarts.py \
-        --out $scratch \
-        --data-dir $SLURM_TMPDIR/data \
-        --resume "$scratch/$exp_id" \
-        --exp-name $exp_id \
-        --no-restarts \
-        --dataset "cifar10" \
-        --n-lbl 4000 \
-        --seed $1 \
-        --split-txt $exp_id \
-        --arch "wideresnet" \
-        --no-progress
-else
-    # Start a new training run
-    python train-cifar-without-restarts.py \
-        --out $scratch \
-        --data-dir $SLURM_TMPDIR/data \
-        --exp-name $exp_id \
-        --no-restarts \
-        --dataset "cifar10" \
-        --n-lbl 4000 \
-        --seed $1 \
-        --split-txt $exp_id \
-        --arch "wideresnet" \
-        --no-progress
-fi
+# if test -d "$scratch/$exp_id"; then
+#     # Resume training run
+#     python train-cifar-without-restarts.py \
+#         --out $scratch \
+#         --data-dir $SLURM_TMPDIR/data \
+#         --resume "$scratch/$exp_id" \
+#         --exp-name $exp_id \
+#         --no-restarts \
+#         --dataset "cifar10" \
+#         --n-lbl 4000 \
+#         --seed $1 \
+#         --split-txt $exp_id \
+#         --arch "wideresnet" \
+#         --no-progress
+# else
+#     # Start a new training run
+#     python train-cifar-without-restarts.py \
+#         --out $scratch \
+#         --data-dir $SLURM_TMPDIR/data \
+#         --exp-name $exp_id \
+#         --no-restarts \
+#         --dataset "cifar10" \
+#         --n-lbl 4000 \
+#         --seed $1 \
+#         --split-txt $exp_id \
+#         --arch "wideresnet" \
+#         --no-progress
+# fi
