94a2b2c486ab34cce6c5127fe1239ec417d70199
Adam Tupper <adam.tupper@outlook.com>
2023-03-01 16:43:36 +0000

    Remove 2000, 500, and 250 label conditions from launch script.

--- scripts/cc_launch_wrn_cifar10_training.sh
+++ scripts/cc_launch_wrn_cifar10_training.sh
@@ -7,13 +7,8 @@ seeds=(960146 663829 225659)
 for seed in "${seeds[@]}"
 do
     echo "Submitting jobs for seed: $seed..."
-    sbatch cc_train_cifar10_250.sh $seed
-    sleep 10
-    sbatch cc_train_cifar10_500.sh $seed
-    sleep 10
+    
     sbatch cc_train_cifar10_1000.sh $seed
-    sleep 10
-    sbatch cc_train_cifar10_2000.sh $seed
     sleep 10
     sbatch cc_train_cifar10_4000.sh $seed
     sleep 10
